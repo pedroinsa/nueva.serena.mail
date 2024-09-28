581011222ea54a017bbd4a0a8b61db99f0df38ad
Pedro Insausti <pedro@pedro-i5>
2024-09-27 21:01:55 -0300

    fix port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./src/index")
 const { PORT } = require("./config/env")
-
+const port = PORT || 3000
 
 server.listen(PORT, () => {
     console.log(`connected on port ${PORT}`)
