468c3775e437cc7c638fc762e48c0dd5a7a967d2
Pedro Insausti <pedro@pedro-i5>
2024-09-29 02:54:11 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
 const routes = require("../routes/index")
 const app = express()
 app.use(morgan("dev"))
 app.use(express.json())
+app.use(cors())
 app.use("/", routes)
 app.use("*", (req, res) => {
     res.status(404).send("Not found")
